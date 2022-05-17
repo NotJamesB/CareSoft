23c286d869599bf76075e55b04c1fdf6a381ae75
NotJamesB <97855508+NotJamesB@users.noreply.github.com>
2022-05-16 17:52:26 -0700

    Add files via upload

---
+++ User.java
@@ -0,0 +1,25 @@
+package com.caresoft.clinicapp;
+
+public class User {
+  protected Integer id;
+  protected int pin;
+  
+  	public User(int id) {
+  		this.id = id;
+  		
+  	}
+  	
+  	public int getId() {
+  		return id;
+  	}
+  	
+  	public void setId(int id) {
+  		this.id = id;
+  	}
+  	public int getPin() {
+  		return pin;
+  	}
+  	public void setPin(int pin) {
+  		this.pin = pin;
+  	}
+}
